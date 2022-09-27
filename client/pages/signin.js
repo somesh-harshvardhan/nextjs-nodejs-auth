0487483dd9d6f9120339361ec8dccee02d3a7e1e
somesh-harshvardhan <somesh.harshvardhan@gmail.com>
2022-09-28 03:00:03 +0530

    kew

--- client/pages/signin.js
+++ client/pages/signin.js
@@ -6,7 +6,7 @@ import FlexBox from '../src/layout-blocks/FlexBox'
 const SignIn = () => {
   return (
     <FlexBox minHeight="100vh" tag='section' alignItems="center">
-        hello
+        hello kekw
     </FlexBox>
   )
 }
